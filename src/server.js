a61cf2c4559f7a5b502a83b40d4084b4c8c1aa62
gksouza <gkoliver2020@gmail.com>
2024-02-04 14:43:15 -0300

    learning method get

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Grace Kelly");
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
