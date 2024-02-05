339472370a411f8c071601bab9977aa4e4b0fb24
gksouza <gkoliver2020@gmail.com>
2024-02-05 10:08:17 -0300

    changed class name

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,10 @@
-class usersController {
+class UsersController {
+  create(request, response) {
+    const { name, email, password } = request.body;
+  
+    response.json({ name, email, password });
+  }
 
 }
 
-module.exports = usersController;
+module.exports = UsersController;
