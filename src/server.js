b22dfc99d9e9d219ba0cb3247e9bfea476e1bb9c
gksouza <gkoliver2020@gmail.com>
2024-02-05 08:20:12 -0300

    body params

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
 
 app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
   
-  response.send( "Você chamou o POST");
+  response.json({ name, email, password });
 });
 
 const PORT = 3333;
