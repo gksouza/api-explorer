ce48671bf9224d5836e63e15b8670ad5957428fd
gksouza <gkoliver2020@gmail.com>
2024-03-01 16:07:47 -0300

    load notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -74,6 +74,7 @@ class NotesController {
         .whereLike("notes.title", `%${title}`)
         .whereIn("name", filterTags)
         .innerJoin("notes", "notes.id", "tags.note_id")
+        .groupBy("notes.id")
         knex.orderBy("notes.title")
 
     } else{
