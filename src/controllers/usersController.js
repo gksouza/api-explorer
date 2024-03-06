7a4e49d030cde33f37c48d554e14ae3a308fa56f
gksouza <gkoliver2020@gmail.com>
2024-03-06 13:20:17 -0300

    using the user repository

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,26 +1,25 @@
-const { hash, compare } = require("bcryptjs");
-const AppError = require("../utils/AppError");
-const sqliteConnection = require("../database/sqlite");
+const { hash, compare } = require("bcryptjs")
+const AppError = require("../utils/AppError")
+const UserRepositoy = require("../repositories/UserRepositoy")
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+const UserRepositoy = require("../repositories/UserRepositoy")
+    const userRepositoy = new UserRepositoy()
+        
+    const checkUserExists = await userRepositoy.findByEmail(email)
 
     if(checkUserExists) {
-      throw new AppError("Este e-mail já está em uso.");
+      throw new AppError("Este e-mail já está em uso.")
     }
 
     const hashedPassword = await hash(password, 8);
+    await userRepositoy.create({name, email, hashedPassword})
 
-    await database.run(
-      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-      [ name, email, hashedPassword ]
-    );
-
-    return response.status(201).json();   
+    return response.status(201).json() 
   }
 
   async update(request, response) {
